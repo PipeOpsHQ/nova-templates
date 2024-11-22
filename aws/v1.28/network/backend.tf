6f92a0cf2db6d95e3b0e206dfc521ab0593d5c55
Alex Idowu <alexidowu25@gmail.com>
2024-11-22 02:02:02 +0100

    Update backend.tf

--- aws/v1.28/network/backend.tf
+++ aws/v1.28/network/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "<replaceAccountID>-owned-by-pipeops-do-not-delete"
-    key            = "<replaceKey>-tf-module-state"
+    bucket         = "owned-by-pipeops-do-not-delete"
+    key            = "tf-module-state"
     region         = "eu-west-2"
     encrypt        = true
   }
