abff0d4bb9e798512f8897cfe022c9141e38ddfd
Alex Idowu <alexidowu25@gmail.com>
2024-11-26 01:19:03 +0100

    Refactor network module configuration for simplicity

--- aws/hippa/network/main.tf
+++ aws/hippa/network/main.tf
@@ -5,16 +5,11 @@ module "network" {
   name = "${var.pipeops_workspace_account}-vpc"
 
   cidr = var.vpc_cidr
-
-  azs = slice(
-    data.aws_availability_zones.available.names,
-    0,
-    min(length(data.aws_availability_zones.available.names), 3)
-  )
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 
   private_subnets = var.private_subnets_cidrs
   public_subnets  = var.public_subnets_cidrs
-
+  
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
@@ -22,15 +17,15 @@ module "network" {
 
   public_subnet_tags = {
     "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                        = 1
-    "subnet"                                        = "public"
-    "map_public_ip_on_launch"                       = true
+    "kubernetes.io/role/elb"                    = 1
+    "subnet"                                    = "public"
+    "map_public_ip_on_launch"                   = true
   }
 
   private_subnet_tags = {
     "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"               = 1
-    "subnet"                                        = "private"
+    "kubernetes.io/role/internal-elb"           = 1
+    "subnet"                                    = "private"
   }
 }
 
