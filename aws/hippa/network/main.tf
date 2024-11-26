6158129407ed9ac6191a5297104e562d03e7f752
Alex Idowu <alexidowu25@gmail.com>
2024-11-26 01:07:56 +0100

    Update main.tf to dynamically set number of AZs in use

--- aws/hippa/network/main.tf
+++ aws/hippa/network/main.tf
@@ -5,11 +5,11 @@ module "network" {
   name = "${var.pipeops_workspace_account}-vpc"
 
   cidr = var.vpc_cidr
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs  = slice(data.aws_availability_zones.available.names, 0, local.az_count)
 
   private_subnets = var.private_subnets_cidrs
   public_subnets  = var.public_subnets_cidrs
-  
+
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
@@ -17,17 +17,23 @@ module "network" {
 
   public_subnet_tags = {
     "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                    = 1
-    "subnet"                                    = "public"
-    "map_public_ip_on_launch"                   = true
+    "kubernetes.io/role/elb"                        = 1
+    "subnet"                                        = "public"
+    "map_public_ip_on_launch"                       = true
   }
 
   private_subnet_tags = {
     "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"           = 1
-    "subnet"                                    = "private"
+    "kubernetes.io/role/internal-elb"               = 1
+    "subnet"                                        = "private"
   }
 }
+
+locals {
+  # Get the number of AZs, but cap it at 3
+  az_count = min(length(data.aws_availability_zones.available.names), 3)
+}
+
 
 data "aws_availability_zones" "available" {
   state = "available"
