0e424902d676d17689bbaabfe785d0ea7ab79a10
Alex Idowu <alexidowu25@gmail.com>
2024-11-27 10:47:16 +0100

    Remove unused k8_config from prometheus-server module

--- aws/v1.28/prepare-cluster/monitoring.tf
+++ aws/v1.28/prepare-cluster/monitoring.tf
@@ -16,7 +16,6 @@ module "metrics-server" {
 
 module "prometheus-server" {
   source           = "./helm/monitoring/prometheus"
-  k8_config        = var.k8_config
   cluster_name     = var.eks_cluster_name
   dns_zone         = var.dns_zone
 }
