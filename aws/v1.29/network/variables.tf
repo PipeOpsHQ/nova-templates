0034bffbb9c661710bb0fe784380d6af17f9feec
Alex Idowu <alexidowu25@gmail.com>
2024-12-10 22:48:56 +0100

    Add intra_subnets_cidrs variable for internal subnets

--- aws/v1.29/network/variables.tf
+++ aws/v1.29/network/variables.tf
@@ -16,6 +16,12 @@ variable "public_subnets_cidrs" {
   default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
 
+variable "intra_subnets_cidrs" {
+  description = "Public Subnets CIDR"
+  type        = list(string)
+  default     = ["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
+}
+
 variable "eks_cluster_name" {
   description = "Name of Cluster"
   type        = string
