51396b59186363f6462b3d7f96e5d0b0ca70c560
Alex Idowu <alexidowu25@gmail.com>
2025-02-27 12:40:03 +0100

    docs: Update output descriptions in service-acc.tf and outputs.tf

--- aws/v1.31/prepare-cluster/outputs.tf
+++ aws/v1.31/prepare-cluster/outputs.tf
@@ -70,3 +70,29 @@ output "kube_grafana_auth_password" {
   sensitive = false
   value     = module.kube-prometheus-stack[0].kube_grafana_auth_password
 }
+
+output "service_account_namespace" {
+  description = "The namespace where the service account is created"
+  value       = kubernetes_namespace.service_account_ns.metadata[0].name
+}
+
+output "service_account_name" {
+  description = "The name of the created service account"
+  value       = kubernetes_service_account.pipeops_admin.metadata[0].name
+}
+
+output "service_account_token" {
+  description = "The token for the service account"
+  value       = kubernetes_secret.pipeops_admin_secret.data["token"]
+  sensitive   = true
+}
+
+output "service_account_secret_name" {
+  description = "The name of the secret containing the service account token"
+  value       = kubernetes_secret.pipeops_admin_secret.metadata[0].name
+}
+
+output "cluster_role_binding_name" {
+  description = "The name of the cluster role binding"
+  value       = kubernetes_cluster_role_binding.pipeops_cluster_role_binding.metadata[0].name
+}
