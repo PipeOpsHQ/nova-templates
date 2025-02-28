34c1e35506df3ef1cd209b255b73eb4491fd1e05
Alex Idowu <alexidowu25@gmail.com>
2025-02-28 02:12:30 +0100

    style: Update variable name in output.tf for consistency

--- aws/v1.30/prepare-cluster/helm/monitoring/grafana-loki/output.tf
+++ aws/v1.30/prepare-cluster/helm/monitoring/grafana-loki/output.tf
@@ -1,6 +1,6 @@
 output "grafana_loki_auth_password" {
   sensitive = false
-  value     = random_string.grafana_loki_password.result
+  value     = random_string.grafana-loki-password.result
 }
 
 output "grafana-loki-host" {
