0fcf7272065c375b9e3f30eb0058593f3cd879ef
Alex Idowu <alexidowu25@gmail.com>
2025-03-02 08:50:15 +0100

    chore: Add dependency on ingress-controller in monitoring and opencost modules

--- aws/v1.30/prepare-cluster/monitoring.tf
+++ aws/v1.30/prepare-cluster/monitoring.tf
@@ -20,7 +20,7 @@ module "kube-prometheus-stack" {
   
   cluster_name     = var.eks_cluster_name
   dns_zone         = var.dns_zone
-  
+  depends_on = [ module.ingress-controller ]
 }
 
 ################ End Configure Kubernetes Monitoring (Kube-Prometheus Stack)  #######################################
