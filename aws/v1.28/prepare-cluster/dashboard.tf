c715ee844319cc03697c8938462502619ec44908
ToluwalopeAyo <tolulopeaiyegbusi@gmail.com>
2025-03-04 11:29:52 +0100

    deleted aws eks v1.28 & v1.29

--- aws/v1.28/prepare-cluster/dashboard.tf
+++
@@ -1,9 +0,0 @@
-################ Configure Kubernetes Dashboard #######################################
-
-module "kubernetes-dashboard" {
-  source           = "./helm/dashboard/kubernetes-dashboard"
-  cluster_name     = var.eks_cluster_name
-  dns_zone         = var.dns_zone
-}
-
-################ End Configure Kubernetes Dashboard  #######################################
--- aws/v1.29/prepare-cluster/helm/monitoring/grafana-loki/output.tf
+++
@@ -1,9 +0,0 @@
-output "grafana_loki_auth_password" {
-  sensitive = false
-  value     = random_string.grafana_loki_password.result
-}
-
-output "grafana-loki-host" {
-  sensitive = false
-  value = "grafana-loki-${var.cluster_name}.${var.dns_zone}"
-}
