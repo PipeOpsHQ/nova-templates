cd5b61d71b104d53f6b2d1c8c60445a8a2e2f1ba
ToluwalopeAyo <tolulopeaiyegbusi@gmail.com>
2025-03-05 10:07:04 +0100

    added provider configuration for opencost module

--- do/prepare-cluster/helm/monitoring/grafana-loki/output.tf
+++ do/prepare-cluster/helm/monitoring/grafana-loki/output.tf
@@ -2,12 +2,12 @@ output "grafana_loki_auth_password" {
   sensitive = false
   value     = random_string.grafana_loki_password.result
 }
-/*
+
 output "grafana_loki_auth_username" {
   sensitive = false
   value     = random_string.grafana_loki_username.result
 }
-*/
+
 output "grafana-loki-host" {
   sensitive = false
   value     = "grafana-loki-${var.cluster_name}.${var.dns_zone}"
