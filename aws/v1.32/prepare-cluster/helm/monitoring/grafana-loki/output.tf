f38714a8d62ae790869cfa93231395cded71b42b
ToluwalopeAyo <tolulopeaiyegbusi@gmail.com>
2025-03-10 19:44:53 +0100

    added grafana-loki-username output

--- aws/v1.32/prepare-cluster/helm/monitoring/grafana-loki/output.tf
+++ aws/v1.32/prepare-cluster/helm/monitoring/grafana-loki/output.tf
@@ -2,6 +2,10 @@ output "grafana_loki_auth_password" {
   sensitive = false
   value     = random_string.grafana-loki-password.result
 }
+output "grafana_loki_auth_username" {
+  sensitive = false
+  value     = random_string.grafana_loki_username.result
+}
 
 output "grafana-loki-host" {
   sensitive = false
